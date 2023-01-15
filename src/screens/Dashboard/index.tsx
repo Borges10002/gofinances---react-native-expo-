692567fd881c7f8c2427e49f28030098e335b21f
unknown <borges10002@gmail.com>
2023-01-14 23:21:57 -0400

    aula syled components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard } from "../../components/TransactionCard";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
+} from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/32522338?v=4.png",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá</UserGreeting>
+              <UserName>Diego Borges</UserName>
+            </User>
+          </UserInfo>
+          <Icon name='power' />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type='up'
+          title='Entradas'
+          amount='R$ 17.400,00'
+          lastTransaction='Última entrada dia 13 de abriel'
+        />
+
+        <HighlightCard
+          type='down'
+          title='Saídas'
+          amount='R$ 1.259,00'
+          lastTransaction='Última saída dia 03 de abriel'
+        />
+
+        <HighlightCard
+          type='total'
+          title='Total'
+          amount='R$ 16.141.00'
+          lastTransaction='01 à 16 de abriel'
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionCard />
+      </Transactions>
+    </Container>
+  );
+}
