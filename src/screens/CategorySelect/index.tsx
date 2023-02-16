678c38557b458397ff246b061e3b77836067f718
Borges - 746 <borges@ciclocairu.com.br>
2023-02-16 07:14:09 -0400

    utilizando modal

--- src/screens/CategorySelect/index.tsx
+++ src/screens/CategorySelect/index.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { FlatList } from "react-native";
+import { Button } from "../../components/Form/Button";
+import { categories } from "../../utils/categories";
 
-import { Container, Header, Title } from "./styles";
+import {
+  Container,
+  Header,
+  Title,
+  Category,
+  Icon,
+  Name,
+  Separator,
+  Footer,
+} from "./styles";
 
 interface Category {
   key: string;
@@ -23,6 +35,22 @@ export function CategorySelect({
       <Header>
         <Title>Categoria</Title>
       </Header>
+      <FlatList
+        data={categories}
+        style={{ flex: 1, width: "100%" }}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <Category>
+            <Icon name={item.icon} />
+            <Name>{item.name}</Name>
+          </Category>
+        )}
+        ItemSeparatorComponent={() => <Separator />}
+      />
+
+      <Footer>
+        <Button title="Selecionar" />
+      </Footer>
     </Container>
   );
 }
