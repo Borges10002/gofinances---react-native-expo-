059889beff4561cb261ecb8ee398345c16ce6dbe
Borges - 746 <borges@ciclocairu.com.br>
2023-03-31 21:00:00 -0400

    aut

--- src/components/SignInSocialButton/styles.ts
+++ src/components/SignInSocialButton/styles.ts
@@ -1,13 +1,38 @@
 import styled from "styled-components/native";
 import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 import { ReactNode } from "react";
+import { RFValue } from "react-native-responsive-fontsize";
 
 interface ContainerProps extends RectButtonProps {
   children: ReactNode;
 }
 
-export const Button = styled(RectButton)<RectButtonProps>``;
+export const Button = styled(RectButton)<RectButtonProps>`
+  height: ${RFValue(56)}px;
 
-export const ImageContainer = styled.View``;
+  background-color: ${({ theme }) => theme.colors.shape};
+  border-radius: 5px;
 
-export const Text = styled.Text``;
+  align-items: center;
+  flex-direction: row;
+
+  margin-bottom: 16px;
+`;
+
+export const ImageContainer = styled.View`
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+
+  padding: ${RFValue(16)}px;
+  border-color: ${({ theme }) => theme.colors.background};
+  border-right-width: 1px;
+`;
+
+export const Text = styled.Text`
+  flex: 1;
+  text-align: center;
+
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(14)}px;
+`;
